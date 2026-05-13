74bf4f4d41db9fd0680ba51687c2ae792e5acb29
Zach Delgros <truegamer95@gmail.com>
2026-05-13 01:14:36 -0700

    Map panning w/ mouse + can place map "inside" dashboard subviewport

--- godot/scenes/camera/interactive_camera.gd
+++ godot/scenes/camera/interactive_camera.gd
@@ -1,11 +1,34 @@
+class_name InteractiveCamera
 extends Camera2D
 
+@export var hide_mouse_while_dragging : bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var mouse_press_position := Vector2.ZERO
+var drag_enabled : bool = false
+var drag_vector := Vector2.ZERO
 
+func _unhandled_input(event: InputEvent) -> void:
+	# START/END DRAG
+	if event is InputEventMouseButton:
+		drag_enabled = event.is_pressed()
+		if event.is_pressed():
+			if not event.is_echo():
+				mouse_press_position = event.position
+				if hide_mouse_while_dragging:
+					Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	# UPDATE DRAG
+	if event is InputEventMouseMotion:
+		if drag_enabled:
+			drag_vector = event.relative
+			# event.relative is not a reliable way to see if movement has stopped
+			# the timer below makes sure it is stopped
+			get_tree().create_timer(0.001).timeout.connect(reset_drag_vector)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if drag_enabled:
+		global_position -= drag_vector
+
+func reset_drag_vector():
+	drag_vector = Vector2.ZERO
