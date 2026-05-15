6a637f82f0e9b08d89f6fbd88a0dca0bf29ee015
Raisinaut <truegamer95@gmail.com>
2026-05-15 00:51:09 -0700

    Hovered vertices are highlighted

--- godot/scenes/game_scene/map/network_vertex.gd
+++ godot/scenes/game_scene/map/network_vertex.gd
@@ -11,24 +11,34 @@ signal rclicked(vertex)
 		$CollisionShape2D.scale = Vector2(val / 10, val / 10)
 		queue_redraw()
 
+@export var highlight_color : Color
+
 var color: Color = Color.WHITE
 
 var selected: bool = false
+var hovered: bool  = false : set = set_hovered
 
 func editor_redraw() -> void:
 	if Engine.is_editor_hint(): queue_redraw()
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, radius, color, true, -1, true)
+	if hovered:
+		draw_circle(Vector2.ZERO, radius * 1.5, highlight_color, true, -1, true)
 	if selected:
 		draw_circle(Vector2.ZERO, radius + 5, Color.WHITE, false, 1, true)
+	draw_circle(Vector2.ZERO, radius, color, true, -1, true)
 
 func _ready() -> void:
 	if Engine.is_editor_hint(): return
-	pass
+	mouse_entered.connect(set_hovered.bind(true))
+	mouse_exited.connect(set_hovered.bind(false))
 
 func _process(_delta: float) -> void:
 	editor_redraw()
+
+func set_hovered(state : bool) -> void:
+	hovered = state
+	queue_redraw()
 
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
