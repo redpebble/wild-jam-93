acee1059d72cb2de588108deb11e30273f0db04a
Jasper Andrew <jqaxyz@gmail.com>
2026-05-16 03:14:01 -0400

    connect game win/lose to debt/moves

--- godot/addons/maaacks_game_template/extras/scripts/win_lose_manager.gd
+++ godot/addons/maaacks_game_template/extras/scripts/win_lose_manager.gd
@@ -70,9 +70,12 @@ func game_won() -> void:
 	_load_win_screen_or_ending()
 
 
-func _on_level_game_lost() -> void:
+func _on_game_won() -> void:
+	game_won()
+
+func _on_game_lost() -> void:
 	game_lost()
 
-
-func _on_level_game_won() -> void:
-	game_won()
+func _ready():
+	PlayerData.game_won.connect(_on_game_won)
+	PlayerData.game_lost.connect(_on_game_lost)
--- godot/scenes/autoload/player_data.gd
+++ godot/scenes/autoload/player_data.gd
@@ -3,17 +3,28 @@ extends Node
 signal moves_updated(moves)
 signal debt_updated(debt)
 
-const starting_moves : int = 50
-const starting_debt : int = 900000
+signal game_won
+signal game_lost
+
+const starting_moves : int = 3 # TESTING
+const starting_debt : int = 69420
 
 var moves_remaining : int = starting_moves :
     set(n):
         moves_remaining = n
         moves_updated.emit(n)
+        checkGameEnd()
 
 var debt_remaining : int = starting_debt :
     set(n):
         debt_remaining = n
         debt_updated.emit(n)
+        checkGameEnd()
 
 var active_contracts : Array[ContractData] = []
+
+func checkGameEnd() -> void:
+    if debt_remaining <= 0:
+        game_won.emit()
+    elif moves_remaining <= 0:
+        game_lost.emit()
