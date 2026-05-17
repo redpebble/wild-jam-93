3515ad30b02a0f7478f7e0f13a76a80ac63436a7
Raisinaut <truegamer95@gmail.com>
2026-05-17 13:26:24 -0700

    More balance tweaks: higher rewards, fewer contracts

--- godot/scenes/autoload/contract_manager.gd
+++ godot/scenes/autoload/contract_manager.gd
@@ -41,8 +41,8 @@ func generate_contract(vertex : NetworkVertex, destination_vertex : NetworkVerte
 	data.destination = destination_vertex
 	# TODO: Make distance based on edge count
 	var distance = destination_vertex.global_position.distance_to(vertex.global_position)
-	data.size = randi_range(2, 8) * 50
-	data.reward = data.size * round(distance * 0.05) # just messing 
+	data.size = randi_range(4, 8) * 50
+	data.reward = data.size * round(distance * 0.07) # just messing 
 	return data
 
 func get_location_contracts(vertex : NetworkVertex) -> Array:
